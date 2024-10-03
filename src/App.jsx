c477005e9c82ae014daa0ad3d0a8e5f5772e898a
lancefinke <lance_finke@comcast.net>
2024-10-02 21:01:52 -0500

    feat:created header elements and added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { Button } from 'react-bootstrap'
 import { useState } from 'react'
-
+import { Container } from 'react-bootstrap'
+import { Stack } from 'react-bootstrap'
+import './styles.css'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Container className='my-4'>
+      <Stack direction='horizontal' gap="2" className='mb-4'>
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
+      </Stack>
+      <div className="budget-grid">
+
+      </div>
+    </Container>
   )
 }
 
